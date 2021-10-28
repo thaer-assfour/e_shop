10211d5192d3115e74606489dd5b2fea4398bc49
Thaer Assfour <thaer.assfour@gmail.com>
2021-10-28 15:19:13 +0300

    add Signup with validator

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -6,13 +6,13 @@ class CustomFormTextField extends StatelessWidget {
   late IconData icon;
   late bool isPassword;
 
-  CustomFormTextField({required this.label,required this.icon,required this.isPassword});
-
+  CustomFormTextField(
+      {required this.label, required this.icon, required this.isPassword});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
       child: TextFormField(
         obscureText: isPassword,
         cursorColor: KFourthColor,
@@ -20,8 +20,14 @@ class CustomFormTextField extends StatelessWidget {
         style: const TextStyle(
             color: KFourthColor, fontSize: 16, fontWeight: FontWeight.w500),
         decoration: InputDecoration(
-          label:  Text(label,style: const TextStyle(fontSize: 18,color: KFourthColor),),
-          prefixIcon: Icon(icon,color: KFourthColor,),
+          label: Text(
+            label,
+            style: const TextStyle(fontSize: 18, color: KFourthColor),
+          ),
+          prefixIcon: Icon(
+            icon,
+            color: KFourthColor,
+          ),
           filled: true,
           fillColor: KSecondColor,
           enabledBorder: OutlineInputBorder(
@@ -36,33 +42,39 @@ class CustomFormTextField extends StatelessWidget {
           focusedErrorBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(16),
               borderSide: const BorderSide(color: Colors.red)),
-          errorStyle: const TextStyle(color: Colors.redAccent,fontWeight: FontWeight.w500),
+          errorStyle: const TextStyle(
+              color: Colors.redAccent, fontWeight: FontWeight.w500),
         ),
-        validator: (value){
-          if(label == 'Name'){
-            if(value!.isEmpty) {
+        validator: (value) {
+          if (label == 'Name') {
+            if (value!.isEmpty) {
               return 'Name can\'t be empty.';
             }
-            if(value.length < 6) {
+            if (value.length < 6) {
               return 'Name should be at least 6 character.';
             }
           }
-          if(label == 'E-mail'){
-            bool emailValid = RegExp(r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$').hasMatch(value!);
-            if(!emailValid) {
+          if (label == 'E-mail') {
+            if (value!.isEmpty) {
+              return 'E-mail is required.';
+            }
+            bool emailValid =
+                RegExp(r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$')
+                    .hasMatch(value);
+            if (!emailValid) {
               return 'Email Not valid.';
             }
           }
-          if(label == 'Password'){
-            if(value!.length < 6) {
+          if (label == 'Password') {
+            if (value!.isEmpty) {
+              return 'Password id required.';
+            }
+            if (value.length < 6) {
               return 'Password should be at least 8 character.';
             }
           }
         },
-
       ),
     );
   }
-
-
 }
